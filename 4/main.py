52eedf11961c023b0bb7cc69a67bbe249aaf2398
Taylor Scott <88039093+Watlow-tscott@users.noreply.github.com>
2023-12-05 14:32:57 -0600

    started work on main function

--- 4/main.py
+++ 4/main.py
@@ -2,7 +2,14 @@ def main():
 
     with open("_input/input.txt", "r") as file:
 
-        asdf = file.read()
+        lotto_scrachers_str = file.read()
+    
+    lotto_scracher_list = lotto_scrachers_str.split('\n')
+
+    for lotto_scracher in lotto_scracher_list:
+        
+
+    print(list_of_cards)
 
 if __name__ == '__main__':
     main()
